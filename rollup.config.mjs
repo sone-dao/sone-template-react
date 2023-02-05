33cb3b1510998bcf4955e05caf4d3d3d56398607
Lon Beshiri <lonbeshiri@gmail.com>
2023-02-05 15:52:38 -0500

    fix: rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,10 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
-import postcss from 'rollup-plugin-postcss'
-
-import terser from '@rollup/plugin-terser'
 import PeerDepsExternalPlugin from 'rollup-plugin-peer-deps-external'
+import postcss from 'rollup-plugin-postcss'
 import packageJson from './package.json' assert { type: 'json' }
 
 export default [
